5bf83f79f237c850d034953543486c40bb64d7f3
Lessurnek <russelkensantos09@gmail.com>
2024-07-03 00:40:26 +0800

    Exam4

---
+++ app/Models/Item.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+    use HasFactory;
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+}
